d49fdd5482fd23cf4b13684166956660700f442f
litanyue <36092701+litanyue@users.noreply.github.com>
2020-05-15 15:36:48 +0800

    Update app-test.py

--- app-test.py
+++ app-test.py
@@ -1,3 +1,4 @@
+'''
 import os
 from datetime import datetime
 from flask import Flask, render_template, request
@@ -22,6 +23,55 @@ def insert():
 	b = datetime.now()
 	print((b-a).seconds)
 	return str((b-a).seconds)
+
+	
+@app.route('/')
+def hello():
+	return render_template("test.html")
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', debug=True)
+'''
+import os
+from flask import Flask, render_template, request
+from jinja2 import Markup
+import redis
+import random
+
+os.system('mknod -m 644 /dev/urandom c 1 9')
+app = Flask(__name__, static_folder="templates")
+redis0 = redis.Redis(host='192.168.20.3', port=6379)
+redis1 = redis.Redis(host='192.168.20.5', port=6379)
+testnumber = 0
+
+@app.route('/insert')
+def insert():
+	number = random.randint(0, 1)
+	global testnumber
+	temp = testnumber + 5
+	
+	if number == 0:
+		try:
+			while testnumber < temp :
+				redis0.set(testnumber,testnumber)
+				testnumber += 1
+			return "redis0"
+		except redis.exceptions.ConnectionError as e:
+			while testnumber < temp :
+				redis1.set(testnumber,testnumber)
+				testnumber += 1
+			return "redis1"
+	else :
+		try:
+			while testnumber < temp :
+				redis1.set(testnumber,testnumber)
+				testnumber += 1
+			return "redis1"
+		except redis.exceptions.ConnectionError as e:
+			while testnumber < temp :
+				redis0.set(testnumber,testnumber)
+				testnumber += 1
+			return "redis0"
 
 	
 @app.route('/')
