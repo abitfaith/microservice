391787b49c64eb8c6f611a74c8360e39d7495238
litanyue <977681262@qq.com>
2020-05-24 10:19:16 +0800

    add the light app

---
+++ app-2.py
@@ -0,0 +1,28 @@
+# -*- coding:utf8 -*-
+import os
+import random
+
+from flask import Flask, render_template, request, jsonify
+from jinja2 import Markup
+
+os.system('mknod -m 644 /dev/urandom c 1 9')
+app = Flask(__name__, static_folder="templates")
+
+# 页面路由
+@app.route("/")
+def index():
+    return render_template("index2.html")
+
+idx = -1
+
+# 接收更新请求，读取，返回数据路由
+@app.route("/lineDynamicData")
+def update_line_data():
+    global idx
+    idx = idx + 1
+    # 返回的是随机数，格式不变，更改数据库中的value值
+    return jsonify({"name": idx, "value": random.randrange(0, 2)})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
---
+++ app-light.py
@@ -0,0 +1,31 @@
+# -*- coding:utf8 -*-
+import os
+import random
+
+import RPi.GPIO as GPIO
+
+from flask import Flask, render_template, request, jsonify
+from jinja2 import Markup
+
+os.system('mknod -m 644 /dev/urandom c 1 9')
+app = Flask(__name__, static_folder="templates")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21,GPIO.IN)
+# 页面路由
+@app.route("/")
+def index():
+    return render_template("index1.html")
+
+idx = -1
+
+# 接收更新请求，读取，返回数据路由
+@app.route("/lineDynamicData")
+def update_line_data():
+    global idx
+    idx = idx + 1
+    # 从光照传感器上读出光照强度
+    return jsonify({"name": idx, "value": GPIO.input(21)})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
